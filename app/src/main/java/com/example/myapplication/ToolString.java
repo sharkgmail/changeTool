85c827bb682c070ef6f0201496a18bd8bffefd8c
PHMSD8146PUB\Shark <apple888jk@gmail.com>
2022-03-14 16:11:11 +0800

    初始化

---
+++ app/src/main/java/com/example/myapplication/ToolString.java
@@ -0,0 +1,18 @@
+package com.example.myapplication;
+
+/**
+ * <pre>
+ *     author  :  shark
+ *     time   : 2022/3/14  16:08
+ *     describe   :
+ *     version:   1.1.1
+ * </pre>
+ */
+public class ToolString {
+
+    public static String getString() {
+        return "不改变内容";
+    }
+
+
+}
